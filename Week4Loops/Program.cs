64b7cda5f966fb7b4e482effcda77cef2e7342c1
Mathias Vapper. <mathias.vapper@tktk.ee>
2024-09-25 23:09:00 +0300

    add week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//FOR-LOOP
+//Dry-principle - Do not repeat yourself
+
+for(int i = 0; i < 5; i++)  //i++ = i + i
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine("Have a nice day.");
+
+
